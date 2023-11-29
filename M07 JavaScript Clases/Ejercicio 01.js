6ae071e1c6678d0c93adc8a7521228ca699b4e2e
adriel <adrielperez227@gmail.com>
2023-11-29 19:56:56 -0300

    modulo 7 completo

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,21 +10,48 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario{
+      constructor(usuario, nombre, email, password) {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+
+      saludar() {
+         return "Hola, mi nombre es " + this.nombre;
+      }
+   }
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+      var saludo = "Hello World!";
+      return saludo;
+   }
+   return Usuario.saludar;
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function(){
+      var letra = this.split('');
+      var reverso = [];
+      for (let i = this.length - 1; i >= 0; i--){
+         reverso.push(letra[i]);
+      }
+      reverso = reverso.join('');
+      return reverso;
+   }
 }
 
-/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+      /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    crearUsuario,
    agregarMetodoPrototype,
--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,20 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre, apellido, edad, domicilio) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+   detalle() {
+      var obj = {};
+      obj.nombre = this.nombre;
+      obj.apellido = this.apellido;
+      obj.edad = this.edad;
+      obj.domicilio = this.domicilio;
+      return obj;
+   }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +27,7 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   return persona = new Persona(nombre, apellido, edad, domicilio);
 }
 
 function agregarMetodo() {
@@ -20,7 +35,11 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function () {
+      return this.nombre + ", " + this.edad + " años";
+   }
 }
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
